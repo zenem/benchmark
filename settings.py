2e45dafc3655bc1f78a5b6553325b8de3488b6fa
ankeshanand <ankeshanand1994@gmail.com>
2014-07-30 00:29:10 +0530

    Add FileUpload API support through Django Tastypie
    Increase Django requirement to 1.5.8 to satisfy tastypie.

--- settings.py
+++ settings.py
@@ -14,9 +14,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'anand',  # Or path to database file if using sqlite3.
-        'USER': 'anand',                      # Not used with sqlite3.
-        'PASSWORD': 'Anand',                  # Not used with sqlite3.
+        'NAME': 'benchmark_db',  # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'bootstrapform',
+    'tastypie',
 )
 
 # A sample logging configuration. The only tangible logging
